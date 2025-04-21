ac744f5c454533f3fb0cb666032ed2f7d196523b
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-04-21 16:42:17 +0300

    Remove full.svg and introduce new SVG components for Circle, Fermer, Lights, and Platform
    - Deleted the `full.svg` image to streamline the asset usage.
    - Added new SVG components: `Circle`, `Fermer`, `Lights`, and `Platform` to enhance the visual presentation and modularity of the application.
    - Updated `RouteComponent` to utilize the new SVG components, improving the overall structure and maintainability of the code.

---
+++ src/components/images/Lights.tsx
@@ -0,0 +1,36 @@
+export const Lights = () => {
+  return (
+    <svg
+      width="375"
+      height="785"
+      viewBox="0 0 375 785"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-auto w-full"
+    >
+      <g filter="url(#filter0_f_6_26)">
+        <circle cx="179.5" cy="392.5" r="192.5" fill="#87D002" />
+      </g>
+      <defs>
+        <filter
+          id="filter0_f_6_26"
+          x="-213"
+          y="0"
+          width="785"
+          height="785"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur stdDeviation="100" result="effect1_foregroundBlur_6_26" />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
