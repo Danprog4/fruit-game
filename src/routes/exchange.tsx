c86fc7837c072e7978f114188258226646e0eda5
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-04-26 19:07:10 +0300

    Adjust padding in Exchange route component for improved layout consistency

--- src/routes/exchange.tsx
+++ src/routes/exchange.tsx
@@ -66,7 +66,7 @@ function RouteComponent() {
   const percentChange = getPercentageChange();
 
   return (
-    <div className="flex h-screen w-full flex-col overflow-hidden px-4 pt-[20px] pb-[300px] text-white">
+    <div className="flex h-screen w-full flex-col overflow-hidden px-4 pt-[50px] pb-[300px] text-white">
       <BackButton onClick={() => window.history.back()} />
       <div className="font-manrope mx-auto mb-[33px] text-center text-2xl font-semibold">
         Обмен
