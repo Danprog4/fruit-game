a5643ddbcd1fd95e066eeaf54d73ff4315c84287
Danprog4 <148579547+Danprog4@users.noreply.github.com>
2025-04-29 16:05:25 +0300

    Add new SQL migration for allianceSeasons table
    - Dropped default values for `seasonCurr`, `seasonStart`, and `seasonEnd` columns in the `allianceSeasons` table to enforce explicit value assignment.
    - Updated journal and snapshot metadata to reflect the new migration version and changes.

---
+++ drizzle/0001_crazy_apocalypse.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "allianceSeasons" ALTER COLUMN "seasonCurr" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "allianceSeasons" ALTER COLUMN "seasonStart" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "allianceSeasons" ALTER COLUMN "seasonEnd" DROP DEFAULT;
