3622e3b12d9677acb95fec012167bc9dc8ce7f33
manylovv <yellowmanylov@gmail.com>
2025-05-01 02:27:32 +0500

    feat: server only

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,15 +1,8 @@
+import "server-only";
+
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
-
-throw new Error(
-  "My custom error with DATABASE_URL: " +
-    process.env.DATABASE_URL +
-    " and dbs urls: " +
-    import.meta.env.DATABASE_URL +
-    import.meta.env.DB_URL +
-    process.env.DB_URL,
-);
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql, schema });
