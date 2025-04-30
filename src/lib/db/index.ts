6344e0722e31ecd71a746c16337daaab19f55b95
manylovv <yellowmanylov@gmail.com>
2025-05-01 02:16:23 +0500

    chore: throw to test 3

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,7 +2,14 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-throw new Error(import.meta.env.DATABASE_URL!);
+throw new Error(
+  "My custom error with DATABASE_URL: " +
+    process.env.DATABASE_URL +
+    " and dbs urls: " +
+    import.meta.env.DATABASE_URL +
+    import.meta.env.DB_URL +
+    process.env.DB_URL,
+);
 
 const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle({ client: sql, schema });
