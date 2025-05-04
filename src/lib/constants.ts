9c67dfff04ffaf39682eef1e4c2610e20e95d462
manylovv <yellowmanylov@gmail.com>
2025-05-05 03:07:18 +0500

    fix: remove error

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,3 @@
 export const WITHDRAWAL_FEE = 0.05; // 5% fee
 
 export const WITHDRAW_CHAT_ID = process.env.WITHDRAW_CHAT_ID as string;
-if (!WITHDRAW_CHAT_ID) throw new Error("WITHDRAW_CHAT_ID is unset");
